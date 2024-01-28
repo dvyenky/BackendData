7905485257812b1de5441cc526665787567b3be2
Vyenkatesh.D <vyenkatesh.d@intellve.com>
2024-01-28 13:49:03 +0530

    BackendAPI

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const jsonFile = require("jsonfile");
+const cors = require('cors');
+
+
+const app = express();
+const port = 8080;
+app.use(cors())
+
+app.get("/data", (req, res) => {
+  jsonFile.readFile("./db.json", (err, data) => {
+    if (err) {
+      console.error("Error reading file:", err);
+      res.status(500).json({ error: "Internal server error" });
+    } else {
+      res.json(data.data); // Assuming your JSON structure has a 'data' property
+    }
+  });
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
